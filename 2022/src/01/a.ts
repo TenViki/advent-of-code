bc25775aa8d0058dc2e70611d492abc2d34d4c3a
TenViki <viktorkomarek28@gmail.com>
2022-12-01 18:56:48 +0100

    :sparkles: Setups debugger

--- 2022/src/01/a.ts
+++ 2022/src/01/a.ts
@@ -1,18 +1,19 @@
 import { InputParser } from "../lib/files";
 
-const inputParser = new InputParser("input.txt");
+const inputParser = new InputParser("src/01/input.txt");
 
 const elves = [];
 let i = 0;
 
 while (inputParser.hasNext()) {
-    const number = inputParser.nextNumber();
-    if (!number) {
-        i++;
-    } else {
-        if (!elves[i]) elves[i] = 0;
-        elves[i] += number;
-    }
+  const number = inputParser.nextNumber();
+  if (!number) {
+    i++;
+  } else {
+    if (!elves[i]) elves[i] = 0;
+    elves[i] += number;
+    debugger;
+  }
 }
 
 console.log(Math.max(...elves));
