1af1da563a8e2b969c75023eb0d2e55206a55f13
TenViki <viktorkomarek28@gmail.com>
2022-12-05 19:34:07 +0100

    :sparkles: Adds day 05

---
+++ 2022/src/05/b.ts
@@ -0,0 +1,40 @@
+import { InputParser } from "../lib/files";
+const parser = new InputParser("in.txt");
+
+const init: string[] = [];
+const stacks: string[][] = [];
+
+// parsing the input
+while (parser.hasNext()) {
+  const line = parser.next();
+  if (!line) break;
+
+  init.push(line);
+}
+
+init.reverse();
+
+for (let i = 0; i < init[0].length; i++) {
+  const c = init[0][i];
+  if (c == " ") continue;
+
+  const stack = [];
+
+  for (let j = 1; j < init.length; j++) {
+    if (init[j][i] == " ") break;
+    stack.push(init[j][i]);
+  }
+
+  stacks.push(stack);
+}
+
+// doing the actual logic
+while (parser.hasNext()) {
+  var [howMany, from, to] = parser.next()!.match(/\d+/g)!.map(Number);
+
+  const fromStack = stacks[from - 1];
+  const moving = fromStack.splice(fromStack.length - howMany);
+  stacks[to - 1].push(...moving);
+}
+
+console.log("Result:", stacks.map((s) => s[s.length - 1]).join(""));
